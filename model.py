fix integer overflow by switching to int64

model.py
@@ -12,9 +12,9 @@ class DummyModelTrain(object):
     '''
 
     def __init__(self, config, float_dtype):
-        self._x = tf.placeholder(tf.int32, shape=[None, None], name='x')
-        self._y = tf.placeholder(tf.int32, shape=[None], name='y')
-        self._subvocab = tf.placeholder(tf.int32, shape=[None], name='subvocab')
+        self._x = tf.placeholder(tf.int64, shape=[None, None], name='x')
+        self._y = tf.placeholder(tf.int64, shape=[None], name='y')
+        self._subvocab = tf.placeholder(tf.int64, shape=[None], name='subvocab')
         
         self._cost = tf.reduce_mean(tf.reduce_sum(self._x, axis=1) - self._y) + tf.reduce_mean(self._subvocab)
         self._train_op = tf.reduce_mean(tf.reduce_sum(self._x, axis=1) - self._y) + tf.reduce_mean(self._subvocab)
@@ -45,9 +45,9 @@ class WSDModelTrain(object):
     """A LSTM WSD model designed for fast training."""
 
     def __init__(self, config, float_dtype):
-        self._sents_val = tf.placeholder(tf.int32, shape=[None])
-        self._subvocabs_val = tf.placeholder(tf.int32, shape=[None])
-        self._indices_val = tf.placeholder(tf.int32, shape=[None, 6])
+        self._sents_val = tf.placeholder(tf.int64, shape=[None])
+        self._subvocabs_val = tf.placeholder(tf.int64, shape=[None])
+        self._indices_val = tf.placeholder(tf.int64, shape=[None, 6])
 
         self._sents = tf.Variable(self._sents_val, name='data_sents', 
                                   trainable=False, collections=[], validate_shape=False)
@@ -60,9 +60,9 @@ class WSDModelTrain(object):
         data = tf.reshape(self._sents[i[0]:i[0]+i[1]*i[2]], (i[1], i[2]))
         self._subvocab = self._subvocabs[i[3]:i[3]+i[4]]
         target_id = i[5]
-        col = tf.random_uniform((1,), maxval=tf.shape(data)[1], dtype=tf.int32)
+        col = tf.random_uniform((1,), maxval=tf.shape(data)[1], dtype=tf.int64)
         self._y = data[:, col[0]]
-        data_tmp = tf.Variable(0, dtype=tf.int32)
+        data_tmp = tf.Variable(0, dtype=tf.int64)
         data_tmp = tf.assign(data_tmp, tf.transpose(data), validate_shape=False)
         col_of_target_ids = tf.fill((tf.shape(data)[0],), target_id)
         self._x = tf.transpose(tf.scatter_nd_update(data_tmp, [col], [col_of_target_ids]))
@@ -146,9 +146,9 @@ class WSDModelEvaluate(object):
     with @WSDModelTrain."""
 
     def __init__(self, config, float_dtype):
-        self._x = tf.placeholder(tf.int32, shape=[None, None], name='x')
-        self._lens = tf.placeholder(tf.int32, shape=[None], name='lens')
-        self._y = tf.placeholder(tf.int32, shape=[None], name='y')
+        self._x = tf.placeholder(tf.int64, shape=[None, None], name='x')
+        self._lens = tf.placeholder(tf.int64, shape=[None], name='lens')
+        self._y = tf.placeholder(tf.int64, shape=[None], name='y')
         
         E_words = tf.get_variable("word_embedding", 
                 [config.vocab_size, config.emb_dims], dtype=float_dtype)
@@ -200,9 +200,9 @@ class WSIModelTrain(WSDModelTrain):
     """A LSTM word sense induction (WSI) model designed for fast training."""
 
     def __init__(self, config, float_dtype, sense_num=4):
-        self._x = tf.placeholder(tf.int32, shape=[None, None], name='x')
-        self._y = tf.placeholder(tf.int32, shape=[None], name='y')
-        self._subvocab = tf.placeholder(tf.int32, shape=[None], name='subvocab')
+        self._x = tf.placeholder(tf.int64, shape=[None, None], name='x')
+        self._y = tf.placeholder(tf.int64, shape=[None], name='y')
+        self._subvocab = tf.placeholder(tf.int64, shape=[None], name='subvocab')
         
         E_words = tf.get_variable("word_embedding", 
                 [config.vocab_size, config.emb_dims], dtype=float_dtype)

prepare-lstm-wsd.py
@@ -80,7 +80,7 @@ class PadFunc(object):
             value_count = sum(1 for s in sents for _ in s)
             size = len(sents) * max_len
         else:
-            arr = np.zeros((len(sents), max_len), dtype=np.int32)
+            arr = np.zeros((len(sents), max_len), dtype=np.int64)
             size = arr.size
             arr.fill(pad_id)
             value_count = 0
@@ -114,7 +114,7 @@ def pad_batches(inp_path, word2id):
                 last_batch = []
     if last_max_len > 0:
         batches.append(pad(last_batch, last_max_len, pad_id))
-    dev_lens = np.array([len(s) for s in dev], dtype=np.int32)
+    dev_lens = np.array([len(s) for s in dev], dtype=np.int64)
     dev_padded = PadFunc()(dev, max(dev_lens), pad_id)
     sys.stderr.write('Dividing and padding... Done.\n')
     sizes = np.array([b.size for b in batches])
@@ -148,11 +148,11 @@ def reduce_vocab(batches, target_id, full_vocab_size):
 def linearize(train_sents, train_vocabs, train_targets):
     assert len(train_sents) == len(train_vocabs) == len(train_targets)
     sys.stderr.write('Flattening... ')
-    flat_sents = np.concatenate([s.ravel() for s in train_sents]).astype(np.int32)
-    flat_vocabs = np.concatenate([v for v in train_vocabs]).astype(np.int32)
+    flat_sents = np.concatenate([s.ravel() for s in train_sents])
+    flat_vocabs = np.concatenate([v for v in train_vocabs])
     sent_start = 0
     vocab_start = 0
-    indices = np.zeros((len(train_sents), 6), dtype=np.int32)
+    indices = np.zeros((len(train_sents), 6), dtype=np.int64)
     for i in range(len(train_sents)):
         indices[i,0] = sent_start
         indices[i,1], indices[i,2] = train_sents[i].shape
@@ -161,10 +161,11 @@ def linearize(train_sents, train_vocabs, train_targets):
         indices[i,4], = train_vocabs[i].shape
         vocab_start += train_vocabs[i].size
         indices[i,5] = train_targets[i]
-    total_size = flat_sents.size+flat_vocabs.size+indices.size
+    assert np.all(indices >= 0) # check for integer overflow
+    total_size = flat_sents.size*4 + flat_vocabs.size*4 + indices.size*8
     sys.stderr.write('Done.\n')
     sys.stderr.write('Training data consumes roughly %.2f GiB.\n' 
-                     %(total_size*4/float(2**30)))
+                     %(total_size/float(2**30)))
     return flat_sents, flat_vocabs, indices
 
 if __name__ == '__main__':

train-lstm-wsi.py
@@ -104,8 +104,8 @@ def load_data():
         local_target_id = batch_vocab.size-1
         sys.stderr.write('Batch #%d vocab: %d (%.2f%%)\n'
                          %(i, batch_vocab.size, batch_vocab.size*100.0/len(vocab)))
-        train_batches.append((sentences.astype(np.int32), 
-                              batch_vocab.astype(np.int32),
+        train_batches.append((sentences.astype(np.int64), 
+                              batch_vocab.astype(np.int64),
                               local_target_id))
     dev = np.load(FLAGS.data_path + '.dev.npz')
     return vocab, train_batches, dev['data'], dev['lens'], target_id