302239b4550289ec6df9bce5197f85d0aaaa42d3
Marten Postma <mcpostma@fs0.cm.cluster>
2017-10-26 23:26:02 +0200

    perform_wsd

--- perform_wsd.py
+++ perform_wsd.py
@@ -14,7 +14,16 @@ parser.add_argument('-v', dest='vocab_path', required=True, help='path to LSTM v
 parser.add_argument('-c', dest='wsd_df_path', required=True, help='input path to dataframe wsd competition')
 parser.add_argument('-s', dest='sense_embeddings_path', required=True, help='path where sense embeddings are stored')
 parser.add_argument('-o', dest='output_path', required=True, help='path where output wsd will be stored')
+parser.add_argument('-r', dest='results', required=True, help='path where accuracy will be reported')
+parser.add_argument('-g', dest='gran', required=True, help='sensekey | synset')
+parser.add_argument('-f', dest='mfs_fallback', required=True, help='True or False')
+
 args = parser.parse_args()
+args.mfs_fallback = args.mfs_fallback == 'True'
+
+
+with open(args.sense_embeddings_path + '.freq', 'rb') as infile:
+    meaning_freqs = pickle.load(infile)
 
 
 def synset2identifier(synset, wn_version):
@@ -71,7 +80,7 @@ def extract_sentence_wsd_competition(row):
     return target_index, sentence_tokens, lemma, pos
 
 
-def score_synsets(target_embedding, candidate_synsets, sense_embeddings, instance_id, lemma, pos):
+def score_synsets(target_embedding, candidate_synsets, sense_embeddings, instance_id, lemma, pos, gran, synset2higher_level):
     """
     perform wsd
 
@@ -85,8 +94,19 @@ def score_synsets(target_embedding, candidate_synsets, sense_embeddings, instanc
     """
     highest_synsets = []
     highest_conf = 0.0
+    candidate_freq = dict()
 
-    for candidate in candidate_synsets:
+    for synset in candidate_synsets:
+        if gran == 'synset':
+            candidate = synset
+            candidate_freq[synset] = meaning_freqs[candidate]
+        elif gran in {'sensekey', 'blc20', 'direct_hypernym'}:
+            candidate = None
+            if synset in synset2higher_level:
+                candidate = synset2higher_level[synset]
+                candidate_freq[synset] = meaning_freqs[candidate]
+                candidate_freq[synset] = meaning_freqs[candidate]
+
         if candidate not in sense_embeddings:
             print('%s %s %s: candidate %s missing in sense embeddings' % (instance_id, lemma, pos, candidate))
             continue 
@@ -95,9 +115,9 @@ def score_synsets(target_embedding, candidate_synsets, sense_embeddings, instanc
         sim = 1 - spatial.distance.cosine(cand_embedding, target_embedding)
 
         if sim == highest_conf:
-            highest_synsets.append(candidate)
+            highest_synsets.append(synset)
         elif sim > highest_conf:
-            highest_synsets = [candidate]
+            highest_synsets = [synset]
             highest_conf = sim
 
     if len(highest_synsets) == 1:
@@ -106,9 +126,12 @@ def score_synsets(target_embedding, candidate_synsets, sense_embeddings, instanc
         highest_synset = highest_synsets[0]
         print('%s %s %s: 2> synsets with same conf %s: %s' % (instance_id, lemma, pos, highest_conf, highest_synsets))
     else:
-        highest_synset = None
-        print('%s: no highest synset' % instance_id)
-    return highest_synset
+        if args.mfs_fallback:
+            highest_synset = candidate_synsets[0]
+            print('%s: no highest synset -> mfs' % instance_id)
+        else:
+            highest_synset = None
+    return highest_synset, candidate_freq
 
 
 # load wsd competition dataframe
@@ -117,7 +140,7 @@ wsd_df = pandas.read_pickle(args.wsd_df_path)
 # add output column
 wsd_df['lstm_output'] = [None for _ in range(len(wsd_df))]
 wsd_df['lstm_acc'] = [None for _ in range(len(wsd_df))]
-
+wsd_df['emb_freq'] = [None for _ in range(len(wsd_df))]
 
 # load sense embeddings
 with open(args.sense_embeddings_path, 'rb') as infile:
@@ -131,7 +154,7 @@ with tf.Session() as sess:  # your session object
     saver = tf.train.import_meta_graph(args.model_path + '.meta', clear_devices=True)
     saver.restore(sess, args.model_path)
     predicted_context_embs = sess.graph.get_tensor_by_name('Model/predicted_context_embs:0')
-    x = sess.graph.get_tensor_by_name('Model/x:0')
+    x = sess.graph.get_tensor_by_name('Model/Placeholder:0')
 
     for row_index, row in wsd_df.iterrows():
         target_index, sentence_tokens, lemma, pos =  extract_sentence_wsd_competition(row)
@@ -143,14 +166,21 @@ with tf.Session() as sess:  # your session object
 
         # load candidate synsets
         synsets = wn.synsets(lemma, pos=pos)
-        candidate_synsets = {synset2identifier(synset, wn_version='30')
-                             for synset in synsets}
+        candidate_synsets = [synset2identifier(synset, wn_version='30')
+                             for synset in synsets]
+
+        # get mapping to higher abstraction level
+        synset2higher_level = dict()
+        if args.gran in {'sensekey', 'blc20', 'direct_hypernym'}:
+            label = 'synset2%s' % args.gran
+            synset2higher_level = row[label]
 
         # perform wsd
         if len(candidate_synsets) >= 2:
-            chosen_synset = score_synsets(target_embedding, candidate_synsets, sense_embeddings, instance_id, lemma, pos)
+            chosen_synset, candidate_freq = score_synsets(target_embedding, candidate_synsets, sense_embeddings, instance_id, lemma, pos, args.gran, synset2higher_level)
         else:
-            chosen_synset = candidate_synsets.pop()
+            chosen_synset = candidate_synsets[0]
+            candidate_freq = dict()
 
         # add to dataframe
         wsd_df.set_value(row_index, col='lstm_output', value=chosen_synset)
@@ -158,6 +188,7 @@ with tf.Session() as sess:  # your session object
         # score it
         lstm_acc = chosen_synset in row['wn30_engs']
         wsd_df.set_value(row_index, col='lstm_acc', value=lstm_acc)
+        wsd_df.set_value(row_index, col='emb_freq', value=candidate_freq)        
         
         if lstm_acc:
             num_correct += 1
@@ -166,6 +197,9 @@ print(num_correct)
 
 # save it
 wsd_df.to_pickle(args.output_path)
+
+with open(args.results, 'w') as outfile:
+    outfile.write('%s' % num_correct)
 
 
 
